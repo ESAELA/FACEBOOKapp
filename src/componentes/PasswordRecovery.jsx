1b52b9599d92f85a4adc6860f04c1a0a9b39309e
SAELA <esamuelchinama@hotmail.com>
2025-01-20 17:07:37 +0200

    colocando background-color e transicão no componente passwordRecovery

--- src/componentes/PasswordRecovery.jsx
+++ src/componentes/PasswordRecovery.jsx
@@ -2,14 +2,14 @@ function PasswordRecovery () {
     return (
         <div>
 
-            <nav className="bg-white flex justify-between h-[60px] shadow-sm ">
+            <nav className="bg-white flex justify-between h-[60px] shadow-lg ">
                     <img 
                         class="fb_logo _8ilh img cursor-pointer w-[150px] ml-5" src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="Facebook">
                     </img>
                 <div className="flex justify-end items-center gap-2 mr-7">
                     <input className="h-10 border w-[200px] focus:outline-none  rounded-md border-gray-300 p-3 focus:border-solid focus:border-blue-600 focus:shadow-lg" type="email" name="" id="" placeholder="E-mail do telémovel" />
                     <input className="h-10 border w-[200px] focus:outline-none rounded-md border-gray-300 p-3 focus:border-solid focus:border-blue-600 focus:shadow-lg" type="password" name="" id="" placeholder="Palavra-passe" />
-                    <a className="flex justify-center items-center bg-blue-600 w-[130px] rounded-md h-10 text-white font-bold ">Iniciar sessão</a>
+                    <a id="IniciarSessao" className="flex justify-center items-center cursor-pointer w-[130px] rounded-md h-10 text-white font-bold ">Iniciar sessão</a>
                     <a className="text-blue-600 hover:underline" href="/passwordrecovery">
                         Esqueceste-te da tua conta?
                     </a>    
@@ -30,7 +30,7 @@ function PasswordRecovery () {
                 </div>
                 <div className="ml-2 p-4 mt-[23px] border-t-[1px] border-gray-200 flex justify-end gap-2">
                     <button className="bg-gray-300 w-[100px] h-9 rounded-md " type="submit">Cancelar</button>
-                    <button className="bg-blue-600 w-[110px] h-9 rounded-md text-white"  type="submit">Pesquisar</button>
+                    <button id="ButtonSearch" className="w-[110px] h-9 rounded-md text-white"  type="submit">Pesquisar</button>
                 </div>
             </div>
         </div>
