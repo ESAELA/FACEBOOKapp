850e7c8c4d93bed9013afe72c0635ee941339aa1
SAELA <esamuelchinama@hotmail.com>
2025-01-23 18:10:14 +0200

    ...

--- src/componentes/NewPage.jsx
+++ src/componentes/NewPage.jsx
@@ -21,35 +21,35 @@ function NewPage () {
                 </div>
             </div>
 
-                <div class="flex">
-                    <div className="bg-white rounded-md shadow-md h-[440px] w-[450px] flex flex-col items-center p-[30px] border-[1px] border-gray-200"> 
-                        <img src="/src/img/biz_illustration.png" alt="" className="w-[170px]" />
-                        <div className="flex flex-col items-center p-[30px] gap-[8px]">
+                <div class="flex gap-5">
+                    <div className="bg-white rounded-md shadow-md h-[470px] w-[480px] flex flex-col items-center border-gray-200"> 
+                        <img src="/src/img/biz_illustration.png" alt="" className="" />
+                        <div className="flex flex-col items-center">
                             <h2 className="font-bold text-[18px]">
                                 Negócio ou marca
                             </h2>
-                            <span className="text-[14px] text-gray-400 text-center w-[380px] ">
+                            <span className="text-gray-400 text-center">
                                 Apresenta os teus produtos e serviços, destaca a tua marca e alcança mais clientes no Facebook.
                             </span>
                         </div> 
-                        <div className="p-[50px] ">
+                        <div className="">
                             <a href="" className="border-[0.1px] border-gray-200 bg-gray-100 p-[5px] text-gray-400 text-sm flex flex-col w-[85px] justify-center items-center h-[25px]" >
                                 Começar
                             </a>
                         </div>
                     </div>
-                    <div className="bg-white rounded-md shadow-md h-[440px] w-[450px] flex flex-col items-center p-[30px] border-[1px] border-gray-200"> 
-                        <img src="/src/img/community_illustration.png" alt="" className="w-[170px]" />
-                        <div className="flex flex-col items-center p-[30px] gap-[8px]">
-                            <h2 className="font-bold text-[18px] mt-[8px] ">
+                    <div className="bg-white rounded-md shadow-md h-[470px] w-[480px] flex flex-col items-center border-gray-200"> 
+                        <img src="/src/img/community_illustration.png" alt="" className="" />
+                        <div className="flex flex-col items-center">
+                            <h2 className="font-bold">
                                 Comunidade ou figura pública
                             </h2>
-                            <span className="text-[14px] text-gray-400 text-center w-[400px]">
+                            <span className="text-gray-400 text-center">
                                 Entra em contacto e partilha com pessoas na tua comunidade, organização, equipa, grupo ou clube..
                             </span>
                         </div> 
-                        <div className="p-[50px] ">
-                            <a href="" className="border-[0.1px] border-gray-200 bg-gray-100 p-[5px] text-gray-400 text-sm flex flex-col w-[85px] justify-center items-center h-[25px]  " >
+                        <div className="">
+                            <a href="" className="border-[0.1px] border-gray-200 bg-gray-100 p-[5px] text-gray-400 text-sm flex flex-col justify-center items-center" >
                                 Começar
                             </a>
                         </div>
